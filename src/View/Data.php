a3d6757c90a28df55a24208512607d511e21403e
Bagus A Priambada <iam.adty@gmail.com>
2020-06-07 23:06:54 +0700

    fix var name

--- src/View/Data.php
+++ src/View/Data.php
@@ -5,22 +5,22 @@ namespace CI4Xpander\AdminLTE\View;
 class Data extends \CI4Xpander\View\Data
 {
     /**
-     * @var \CI4Xpander\AdminLTE\View\Data\Site
+     * @var \CI4Xpander\AdminLTE\View\Data\Site $site
      */
     public $site;
 
     /**
-     * @var \CI4Xpander\AdminLTE\View\Data\Page
+     * @var \CI4Xpander\AdminLTE\View\Data\Page $page
      */
     public $page;
 
     /**
-     * @var \CI4Xpander\AdminLTE\View\Data\User
+     * @var \CI4Xpander\AdminLTE\View\Data\User $user
      */
     public $user;
 
     /**
-     * @var \CI4Xpander\AdminLTE\View\Data\Template
+     * @var \CI4Xpander\AdminLTE\View\Data\Template $template
      */
     public $template;
 
