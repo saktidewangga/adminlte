c25c76915feeed070a54d2ff29f12f289266fac5
Bagus A Priambada <iam.adty@gmail.com>
2020-07-02 15:08:06 +0700

    dropdown options helper

---
+++ src/View/Component/Form/Dropdown.php
@@ -0,0 +1,33 @@
+<?php namespace CI4Xpander_AdminLTE\View\Component\Form;
+
+class Dropdown
+{
+    public static function buildOptions($data = null, $options = null)
+    {
+        $result = [
+            0 => ''
+        ];
+
+        if (!is_null($data)) {
+            $value = 'id';
+            $label = 'label';
+
+            if (!is_null($options)) {
+                $value = $options['value'] ?? 'id';
+                $label = $options['label'] ?? 'label';
+            }
+
+            array_walk($data, function ($item) use (&$result, $value, $label) {
+                if (is_object($item)) {
+                    $result[$item->{$value}] = $item->{$label};
+                } else {
+                    $result[$item->{$value}] = $item[$label];
+                }
+            });
+
+            return $result;
+        }
+
+        return [];
+    }
+}
