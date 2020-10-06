b98a7bc3b3a053b4ab7404f73a9614c4f4335363
Bagus A Priambada <iam.adty@gmail.com>
2020-10-06 14:57:28 +0700

    data table searchable, orderable, & loading

--- src/Views/Script/DataTable.php
+++ src/Views/Script/DataTable.php
@@ -19,7 +19,6 @@ var <?= $id; ?>Var = $('#<?= $id; ?>').DataTable({
     sDom: '<"row"<"col-sm-6"l><"col-sm-6"f>><"row"<"col-sm-5"i><"col-sm-7"p>><"row"<"col-sm-12"tr>><"row"<"col-sm-5"i><"col-sm-7"p>>',
     sPaginationType: "full_numbers",
     orderMulti: true,
-    searchDelay: 3000,
     processing: true,
     serverSide: true,
     ajax: window.location.href + "/data",
@@ -28,8 +27,8 @@ var <?= $id; ?>Var = $('#<?= $id; ?>').DataTable({
         {
             data: "<?= is_array($column) ? $field : (is_numeric($field) ? $column : $field); ?>",
             name: "<?= is_array($column) ? (isset($column['label']) ? $column['label'] : ucwords(str_replace('_', ' ', $field))) : (is_numeric($field) ? ucwords(str_replace('_', ' ', $column)) : $column); ?>",
-            searchable: true,
-            orderable: true
+            searchable: <?= is_array($column) ? (isset($column['searchable']) ? ($column['searchable'] ? 'true' : 'false') : 'true') : 'true' ?>,
+            orderable: <?= is_array($column) ? (isset($column['orderable']) ? ($column['orderable'] ? 'true' : 'false') : 'true') : 'true' ?>
         }<?= $i < count($columns) ? ',' : ''; ?>
         <?php $i++; endforeach; ?>
     ]
